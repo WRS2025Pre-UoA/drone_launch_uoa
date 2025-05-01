e9074e0f1db3ea2686ca7a00b43d9be3b4fe2838
rui323 <ruiiwata837@gmail.com>
2025-05-01 21:02:30 +0900

    add namespace part

--- launch/bringupP2.launch.py
+++ launch/bringupP2.launch.py
@@ -3,27 +3,32 @@ from launch_ros.actions import Node, ComposableNodeContainer, LoadComposableNode
 from launch_ros.descriptions import ComposableNode
 
 def generate_launch_description():
+    namespace_1 = "drone"
     container = ComposableNodeContainer(
         name="drone_uoa", 
         package="rclcpp_components",
         executable="component_container_mt",#マルチスレッドの場合component_container_mt,シングルはcomponent_container
-        namespace="P2",
+        namespace="DRONE_P2",
         composable_node_descriptions=[
+            
             ComposableNode(
                 package="drone_operation",
                 plugin="component_operator_gui::DroneGUI",
                 name="drone_gui",
+                namespace=namespace_1,
                 extra_arguments=[{"use_intra_process_comms": True}],
                 parameters=[{"mode": "P2"},
                             {"check_duration_sec": 1.0},
                             {"timer_interval_ms": 200}],
                 # remappings=[("raw_image" , "/arm_camera/realsense2_camera_node/color/image_raw")]
-                remappings=[("raw_image" , "/camera/camera/color/image_raw")]#テスト用
+                remappings=[("raw_image" , "/camera/camera/color/image_raw"),
+                            ("qr_image","/drone/qr_image"),]#テスト用
             ),
             ComposableNode( # ここにmisora2_dt_clientを入力
                 package="misora2_dt_client",
                 plugin="dt_client_component::DTClient",
                 name="dt_client",
+                namespace=namespace_1,
                 extra_arguments=[{"use_intra_process_comms": True}],
                 parameters=[{"mode": "P2"}]
             ),
@@ -31,12 +36,14 @@ def generate_launch_description():
                 package="misora2_qr",
                 plugin="component_qr::DetectQR",
                 name="qr",
+                namespace=namespace_1,
                 extra_arguments=[{"use_intra_process_comms": True}],
             ),
             ComposableNode(
                 package="misora2_pressure",
                 plugin="component_pressure::PressureMeasurement",
                 name="pressure",
+                namespace=namespace_1,
                 extra_arguments=[{"use_intra_process_comms": True}],
             )
         ],
--- launch/bringupP6.launch.py
+++ launch/bringupP6.launch.py
@@ -3,16 +3,18 @@ from launch_ros.actions import Node, ComposableNodeContainer, LoadComposableNode
 from launch_ros.descriptions import ComposableNode
 
 def generate_launch_description():
+    namespace_1 = "drone"
     container = ComposableNodeContainer(
         name="drone_uoa", 
         package="rclcpp_components",
         executable="component_container_mt",#マルチスレッドの場合component_container_mt,シングルはcomponent_container
-        namespace="P6",
+        namespace="DRONE_P6",
         composable_node_descriptions=[
             ComposableNode(
                 package="drone_operation",
                 plugin="component_operator_gui::DroneGUI",
                 name="drone_gui",
+                namespace=namespace_1,
                 extra_arguments=[{"use_intra_process_comms": True}],
                 parameters=[{"mode": "P6"},
                             {"check_duration_sec": 1.0},
@@ -24,6 +26,7 @@ def generate_launch_description():
                 package="misora2_dt_client",
                 plugin="dt_client_component::DTClient",
                 name="dt_client",
+                namespace=namespace_1,
                 extra_arguments=[{"use_intra_process_comms": True}],
                 parameters=[{"mode": "P6"}]
             ),
@@ -31,12 +34,14 @@ def generate_launch_description():
                 package="misora2_qr",
                 plugin="component_qr::DetectQR",
                 name="qr",
+                namespace=namespace_1,
                 extra_arguments=[{"use_intra_process_comms": True}],
             ),
             ComposableNode(
                 package="misora2_pressure",
                 plugin="component_pressure::PressureMeasurement",
                 name="pressure",
+                namespace=namespace_1,
                 extra_arguments=[{"use_intra_process_comms": True}],
             )
         ],
